60cfc1a7eaff50b0f9005641a7f15202acb57bfa
Esra Arı <ariesra92@gmail.com>
2022-03-13 13:56:07 +0300

    update process_data

--- data/process_data.py
+++ data/process_data.py
@@ -1,16 +1,53 @@
 import sys
-
+import pandas as pd 
+import numpy as np 
+import sqlite3
+from sqlalchemy import create_engine
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    messages.merge(categories, how='inner', on='id')
+    return df
 
 
 def clean_data(df):
-    pass
+    # create a dataframe of the 36 individual category columns
+    categories = df['categories'].str.split(';', expand=True)
+    # select the first row of the categories dataframe
+    row = categories.head(1)
+
+    # use this row to extract a list of new column names for categories.
+    # one way is to apply a lambda function that takes everything 
+    # up to the second to last character of each string with slicing
+    category_colnames = row.applymap(lambda x: x[:-2]).iloc[0,].tolist()
+
+    # rename the columns of `categories`
+    categories.columns = category_colnames
+
+    #Iterate through the category columns in df to keep only the last character of each string (the 1 or 0).
+    for column in categories:
+        # set each value to be the last character of the string
+        categories[column] = categories[column].astype(str).str[-1]
+        
+        # convert column from string to numeric
+        categories[column] = categories[column].astype(int)
+
+    # drop the original categories column from `df`
+    df = df.drop(['categories'], axis=1)
+
+    # concatenate the original dataframe with the new `categories` dataframe
+    df = pd.concat([df,categories], axis = 1, join = 'inner')
+
+    # drop duplicates
+    df.drop_duplicates(inplace = True)
+
+    return df
 
 
 def save_data(df, database_filename):
-    pass  
+    engine = create_engine('sqlite:///{}'.format(database_filename))
+    df.to_sql('cleaned_disaster_data', engine, index=False)
 
 
 def main():
